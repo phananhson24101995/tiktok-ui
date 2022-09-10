aec4c39808b68fa7a896e7a02057655ab8571561
SonPA <phananhson24101995@gmail.com>
2022-09-11 00:51:17 +0700

    Xây Dựng UI Phần Header 1

---
+++ src/assets/images/index.js
@@ -0,0 +1,3 @@
+export { default as LogoIcon } from './LogoIcon';
+export { default as CloseIcon } from './CloseIcon';
+export { default as SearchIcon } from './SearchIcon';
--- src/components/Layouts/components/Header/index.js
+++ src/components/Layouts/components/Header/index.js
@@ -1,5 +1,8 @@
 import classNames from 'classnames/bind';
 import styles from './Header.module.scss';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faCircleXmark, faSpinner } from '@fortawesome/free-solid-svg-icons';
+import { LogoIcon, CloseIcon, SearchIcon } from "~/assets/images";
 
 const cx = classNames.bind(styles); // Giúp cho việc viết class name có dấu gách ngang
 
@@ -7,7 +10,25 @@ function Header() {
     return (
         <header className={cx('wraper')}>
             <div className={cx('inner')}>
+                <div className={cx('header__logo')}>
+                    <LogoIcon />
+                </div>
+                <div className={cx('header__center')}>
+                    <input placeholder='Tìm kiếm tài khoản và video' spellCheck={false} />
+                    <div className={cx('icon-close')}>
+                        <span>
+                            <CloseIcon />
+                        </span>
 
+                        <span>
+                            <FontAwesomeIcon icon={faSpinner} className={cx('loading')} />
+                        </span>
+                    </div>
+                    <button className={cx('btn-search')}>
+                        <SearchIcon />
+                    </button>
+                </div>
+                <div className={cx('header__right')}></div>
             </div>
         </header>
     );
